f2021e2b9e7841b066533dfdd3b4531aa7548fb9
Chuey <jabarri1@gmail.com>
2016-05-04 10:54:44 -0700

    Cleaned Up & Added tals

--- lib/armory_bot/commands/help.rb
+++ lib/armory_bot/commands/help.rb
@@ -2,15 +2,17 @@ module ArmoryBot
   module Commands
     module Help
       extend Discordrb::Commands::CommandContainer
-      command(:help) do |event|
+      command(:help, bucket: :fight) do |event|
         puts 'CMD: help'
         event << 'Regions: us, eu'
         event << '**?status** [realm] [region]: checks realm status'
-        event << '**?char** [realm] [character] [region]: Shows summary of a character in text format'
-        event << '**?ilvl** [realm] [character] [region]: checks item level of a character.'
-        event << '**?gear** [realm] [character] [region]: Lists Armory Link, Item Level, and Gear with wowhead links. *Does this in a private message*'
-        event << '**?prog** [realm] [character] [region]: Returns character progression in HFC. *Does this in a private message*'
-        event << '**?info**: Gives a bit of quick info about the bot. *Does so in a private message*'
+        event << '**?char** [realm] [character] [region]: Shows summary of a character in text format(Name, Class, Race, Gender, achievement points, mounts, and pets)'
+        event << '**?ilvl** [realm] [character] [region]: Quickly check the item level of a character.'
+        event << '**?gear** [realm] [character] [region]: Lists Armory Link, Item Level, and Gear with wowhead links. *Private message*'
+        event << '**?prog** [realm] [character] [region]: Returns character progression in HFC. *Private message*'
+        event << '**?tals** [realm] [character] [region]: Gets Talents & Glyphs of the character. *Private message*'
+        event << 'Example ?command Aerie Peak Chuey us'
+        event << '**?info**: Gives a bit of quick info about the bot. *Private message*'
         #event << '**?duel/?fight** [@user] [@user]: Randomly assigns a class to each user, and then they duel to the death!'
         event << '**Rate Limits**: 2 calls every 60secs'# for all but Duel command. Duels are 1 every 5mins'
       end
