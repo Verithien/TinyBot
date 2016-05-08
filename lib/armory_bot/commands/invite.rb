cf5770af7576e419be62ab03fbe1f697c1396b44
Chuey <jabarri1@gmail.com>
2016-05-08 04:38:46 -0700

    invite to PM instead of chat

--- lib/armory_bot/commands/invite.rb
+++ lib/armory_bot/commands/invite.rb
@@ -4,9 +4,9 @@ module ArmoryBot
       extend Discordrb::Commands::CommandContainer
       command(:invite) do |event|
         puts 'CMD: INVITE MY BOT'
-        event << "If you'd like to add this bot to your server, clink the link below"
-        event << "Only users with `manage server` permission are able to invite me."
-        event << "<https://discordapp.com/oauth2/authorize?&client_id=169952497690083328&scope=bot&permissions=3200>"
+        event.user.pm """If you'd like to add this bot to your server, clink the link below
+        Only users with `manage server` permission are able to invite me.
+        <https://discordapp.com/oauth2/authorize?&client_id=169952497690083328&scope=bot&permissions=3200>"""
       end
     end
   end
