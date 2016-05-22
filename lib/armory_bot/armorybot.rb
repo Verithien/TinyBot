5659cd8dc5482d9a70d8f9f5a22409efcd001e7f
Chuey <jabarri1@gmail.com>
2016-05-22 06:44:52 -0700

    oops

--- lib/armory_bot/armorybot.rb
+++ lib/armory_bot/armorybot.rb
@@ -31,7 +31,7 @@ module ArmoryBot
 	bot.game = 'Half Life 3'
 	bot.profile.username = 'Armory Bot'
 	bot.profile.avatar = File.new('/app/lib/armory_bot/wow_icon.jpg')
-	puts bot.servers.name
+	puts bot.servers
 	bot.sync
 
 end
