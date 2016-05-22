c0c2878e70102924a00e02fd98b62f258a044638
Chuey <jabarri1@gmail.com>
2016-05-22 06:19:50 -0700

    RWAR

--- lib/armory_bot/commands/serverlist.rb
+++ lib/armory_bot/commands/serverlist.rb
@@ -3,11 +3,11 @@ module ArmoryBot
     module ServerList
       extend Discordrb::Commands::CommandContainer
       command(:serverlist) do |event|
-        #if event.user.id == '100311929821626368'
-          event.user.id
-        #else
-          #nil
-        #end
+        if event.user.id == 100311929821626368
+          event.respond "TEST WORKED"
+        else
+          nil
+        end
       end
     end
   end
