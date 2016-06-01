d8bc07b23450756118c368a7cd113aab4da3c67f
Chuey <jabarri1@gmail.com>
2016-05-31 22:35:34 -0700

    t

--- lib/armory_bot/commands/game.rb
+++ lib/armory_bot/commands/game.rb
@@ -3,8 +3,9 @@ module ArmoryBot
     module Game
       extend Discordrb::Commands::CommandContainer
       command(:game, min_args: 1) do |event, *text|
-      	event.message.delete if bot_profile.permission?(:manage_messages, event.channel)
         event.bot.game = text.join(' ') if event.author.id == 100311929821626368
+        nil
+        event.message.delete if bot_profile.permission?(:manage_messages, event.channel)
         nil
       end
     end
