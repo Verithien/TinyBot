8d04552b2031accc825704b9f916bd527993f9b3
Chuey <jabarri1@gmail.com>
2016-06-04 06:18:29 -0700

    check

--- lib/armory_bot/commands/servers.rb
+++ lib/armory_bot/commands/servers.rb
@@ -2,9 +2,9 @@ module ArmoryBot
   module Commands
     module Servers
       extend Discordrb::Commands::CommandContainer
-      command(:servercount) do |event|
+      command(:servers) do |event|
         break unless event.user.id == 100311929821626368
-        event.bot.servers.count
+        event << event.bot.servers.count
       end
     end
   end
