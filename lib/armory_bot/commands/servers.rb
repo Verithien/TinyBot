31e624805c96167f15c0036e3bcbe492aa5e464c
Chuey <jabarri1@gmail.com>
2016-06-04 06:12:03 -0700

    f

--- lib/armory_bot/commands/servers.rb
+++ lib/armory_bot/commands/servers.rb
@@ -3,6 +3,7 @@ module ArmoryBot
     module Servers
       extend Discordrb::Commands::CommandContainer
         break unless event.user.id == 100311929821626368
+        event.bot.servers.count
       end
     end
   end
