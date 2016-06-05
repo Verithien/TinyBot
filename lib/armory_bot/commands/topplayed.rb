b7d85656dd0e4d1f614f87ab09a6508c3f839614
Chuey <jabarri1@gmail.com>
2016-06-05 14:32:51 -0700

    added time :)

--- lib/armory_bot/commands/topplayed.rb
+++ lib/armory_bot/commands/topplayed.rb
@@ -26,8 +26,6 @@ module ArmoryBot
         pc = HTTParty.get("https://playoverwatch.com/en-us/career/#{platform}/#{region}/#{acc}", :verify => false ).parsed_response
         console = HTTParty.get("https://playoverwatch.com/en-us/career/#{platform}/#{acc}", :verify => false ).parsed_response
 
-        puts "https://playoverwatch.com/en-us/career/#{platform}/#{acc}"
-
         if platform == "pc"
           page = pc
         elsif platform == "xbl" || "psn"
@@ -40,19 +38,30 @@ module ArmoryBot
         parse_page = Nokogiri::HTML(page)
 
         stats = []
+        playt = []
 
         puts "So far so good"
 
         topplayed = parse_page.css('.bg-crystal-dark').css('.content-box').css('.content-box').css('.row').css('.row').css('.progress-category').css('.progress-2').css('.title')
+        timeplayed = parse_page.css('.bg-crystal-dark').css('.content-box').css('.content-box').css('.row').css('.row').css('.progress-category').css('.progress-2').css('.description')
 
         topplayed.map do |a|
           post_name = a.text
           stats.push(post_name)
         end
 
+        timeplayed.map do |a|
+          post_name = a.text
+          playt.push(post_name)
+        end
+
         one = stats[0]
         two = stats[1]
         three = stats[2]
+
+        onet = playt[0]
+        twot = playt[1]
+        threet = playt[0]
 
         if platform == "pc"
           name = account.first
@@ -62,9 +71,9 @@ module ArmoryBot
         end
 
         event << "**#{event.user.mention} - #{name.capitalize} - Top Played**"
-        event << "**#1:** #{one}"
-        event << "**#2:** #{two}"
-        event << "**#3:** #{three}"
+        event << "**#1:** #{one} - #{onet}" 
+        event << "**#2:** #{two} - #{twot}"
+        event << "**#3:** #{three} - #{threet}"
         
       end
     end
