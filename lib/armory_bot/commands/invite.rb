ec9de796f92c7cadfe296b7814f8da65643019a0
jabarri1 <jabarri1@gmail.com>
2016-06-11 03:02:34 -0700

    change invite response

--- lib/armory_bot/commands/invite.rb
+++ lib/armory_bot/commands/invite.rb
@@ -4,7 +4,7 @@ module ArmoryBot
       extend Discordrb::Commands::CommandContainer
       command(:invite) do |event|
         puts 'CMD: INVITE MY BOT'
-        event.user.pm """If you'd like to add this bot to your server, clink the link below
+        event.respond """If you'd like to add this bot to your server, clink the link below
         Only users with `manage server` permission are able to invite me.
         <https://discordapp.com/oauth2/authorize?&client_id=169952497690083328&scope=bot&permissions=3200>
 
