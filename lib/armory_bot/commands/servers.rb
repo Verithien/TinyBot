1bc52bbf20cd37f80f368b69e32e509b6a999f6f
Chuey <jabarri1@gmail.com>
2016-06-22 20:35:36 -0700

    try this shit

--- lib/armory_bot/commands/servers.rb
+++ lib/armory_bot/commands/servers.rb
@@ -6,7 +6,15 @@ module ArmoryBot
 
         break unless event.user.id == 100311929821626368
 
+        HTTParty.post("https://www.carbonitex.net/discord/data/botdata.php",
+        				:body => { :key => "chuey0187s276sazzf",
+        						   :servercount => event.bot.servers.count }.to_json
+        			)
+
+        puts "server count"
+
         event.bot.servers.count
+
       end
     end
   end
