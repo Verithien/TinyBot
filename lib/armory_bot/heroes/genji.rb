c1dd3117adbca11924610bd56f1344a91b0ec6f4
Chuey <jabarri1@gmail.com>
2016-06-24 14:05:06 -0700

    tst

--- lib/armory_bot/heroes/genji.rb
+++ lib/armory_bot/heroes/genji.rb
@@ -15,7 +15,7 @@ module ArmoryBot
           acc = acc.split(' ')
           acc = acc.join('%20')
         else
-          acc = account
+          acc = account.first
         end
 
         if platform == "pc" && region == "us"
