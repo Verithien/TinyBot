02142834cdee440f8f324eb14fcfb50d4c4a1c91
Chuey <jabarri1@gmail.com>
2016-07-16 03:57:14 -0700

    arena
    attempt 2

--- lib/armory_bot/commands/arena.rb
+++ lib/armory_bot/commands/arena.rb
@@ -22,26 +22,26 @@ module ArmoryBot
         end
 
     		arena2v2 = arena["pvp"]["brackets"]["ARENA_BRACKET_2v2"]
-    		arena2v2r = arena2v2["rating"]
+    		arena2v2r = "#{arena2v2["rating"]}"
     		arena2v2w = arena2v2["seasonWon"]
     		#arena2v2l = arena2v2["seasonLost"]
 
     		arena3v3 = arena["pvp"]["brackets"]["ARENA_BRACKET_3v3"]
-    		arena3v3r = arena3v3["rating"]
+    		arena3v3r = "#{arena3v3["rating"]}"
     		arena3v3w = arena3v3["seasonWon"]
     		#arena3v3l = arena3v3["seasonLost"]
 
     		arena5v5 = arena["pvp"]["brackets"]["ARENA_BRACKET_5v5"]
-    		arena5v5r = arena5v5["rating"]
+    		arena5v5r = "#{arena5v5["rating"]}"
     		arena5v5w = arena5v5["seasonWon"]
     		#arena5v5l = arena5v5["seasonLost"]
 
     		rbg = arena["pvp"]["brackets"]["ARENA_BRACKET_RBG"]
-    		rbgr = rbg["rating"]
+    		rbgr = "#{rbg["rating"]}"
     		rbgw = rbg["seasonWon"]
     		#rbgl = rbg["seasonLost"]
 
-        if arena2v2r == 0
+        if arena2v2r.length == 1
           def pad2( random )
               "     " + random
           end
@@ -49,7 +49,7 @@ module ArmoryBot
           def pad2( random )
               "    " + random
           end
-        elsif arena2v2r.length = 3
+        elsif arena2v2r.length == 3
           def pad2( random )
               "   " + random
           end
@@ -67,7 +67,7 @@ module ArmoryBot
           def pad3( random )
               "    " + random
           end
-        elsif arena3v3r.length = 3
+        elsif arena3v3r.length == 3
           def pad3( random )
               "   " + random
           end
@@ -85,7 +85,7 @@ module ArmoryBot
           def pad5( random )
               "    " + random
           end
-        elsif arena5v5r.length = 3
+        elsif arena5v5r.length == 3
           def pad5( random )
               "   " + random
           end
@@ -103,7 +103,7 @@ module ArmoryBot
           def padr( random )
               "    " + random
           end
-        elsif rbgr.length = 3
+        elsif rbgr.length == 3
           def padr( random )
               "   " + random
           end
