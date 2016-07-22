1f27d097cbbea73de1f2f291978245c13656b97f
Chuey <jabarri1@gmail.com>
2016-07-22 00:37:01 -0700

    YAY
    YAY

--- lib/armory_bot/hots/mmr.rb
+++ lib/armory_bot/hots/mmr.rb
@@ -169,18 +169,18 @@ module ArmoryBot
           if data1.length == 0
             event << "No matches found for this account"
           elsif data1[0] != nil && data1[1] != nil && data1[2] != nil
-            event.respond """#{account.to_s} - MMR
+            event.respond """**#{account[0]} | MMR**
 Quick Match: `#{qmmmr}` | `#{quick_match["CurrentMMR"]}`
 Hero League: `#{hlmmr}` | `#{hero_league["CurrentMMR"]}`
 Team League: `#{tlmmr}` | `#{team_league["CurrentMMR"]}`
 """
           elsif data1[0] != nil && data1[1] != nil
-            event.respond """#{account.to_s} - MMR
+            event.respond """**#{account[0]} | MMR**
 Quick Match: `#{qmmmr}` | `#{quick_match["CurrentMMR"]}`
 Hero League: `#{hlmmr}` | `#{quick_match["CurrentMMR"]}`
 """
           elsif data1[0] != nil
-          event.respond """#{account.to_s} - MMR
+          event.respond """**#{account[0]} | MMR**
 Quick Match: `#{qmmmr}` | `#{quick_match["CurrentMMR"]}`
 """
           else
