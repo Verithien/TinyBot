f96eb20970536afcdfbd7c11055f0de473d11a30
jabarri1 <jabarri1@gmail.com>
2016-08-16 13:33:49 -0700

    help update

--- lib/armory_bot/commands/help.rb
+++ lib/armory_bot/commands/help.rb
@@ -8,8 +8,7 @@ module ArmoryBot
 
         puts '#{event.server.name} - CMD: help'
 
-        event << 'Check out ArmoryBot Docs: <http://bit.ly/1R3iaaX>'
-        event << '`Paypal Donation:`<http://bit.ly/1RTGoox> | `Patreon:` <https://www.patreon.com/blizzardbot>'
+        event << 'Bot Website v1: http://bit.ly/2blbFZ2'
         
       end
     end
